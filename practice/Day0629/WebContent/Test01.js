d68f214dab411d4aa96f6cdbeddad1027f44575e
wjjeon91 <wjjeon91@gmail.com>
2020-07-28 13:08:42 +0900

    Today What I Learn
    TIL

---
+++ practice/Day0629/WebContent/Test01.js
@@ -0,0 +1,28 @@
+console.log("world!");
+
+var num1 = 3;
+var num2 = 3.14;
+
+console.log("num1: " + num1);
+console.log("typeof num1: " + typeof num1);
+
+console.log("num2: " + num2);
+console.log("typeof num2: " + typeof num2);
+
+var num3 = Infinity;
+var num4 = NaN;
+
+console.log("num3: " + num3);
+console.log("typeof num3: " + typeof num3);
+
+console.log("num4: " + num4);
+console.log("typeof num4: " + typeof num4);
+
+var str1 = "Hello World!";
+var str2 = 'He says, "JavaScript is fun!"';
+
+console.log("str1: " + str1);
+console.log("typeof str1: " + typeof str1);
+
+console.log("str2: " + str2);
+console.log("typeof str2: " + typeof str2);
